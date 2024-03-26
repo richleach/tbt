788859251edb6756c56cf2c8c458bbcd9c009ea7
Rich Leach <rich@cfsnap.com>
2024-03-25 19:19:47 -0600

    added groupShot01.png to ride with us page, fixed some stupid non-escaped chars

--- app/ride-with-us/page.tsx
+++ app/ride-with-us/page.tsx
@@ -17,31 +17,32 @@ export default function rideWithUs() {
         <div className="pl-3" style={{ borderLeft: "thin solid #EA580C" }}>
           <h2 style={{ borderBottom: "thin solid #ea580c" }}>Ride With Us</h2>
           <div
-            className="p-2 mb-4 mt-4 text-black bg-white border-2 rounded-lg md:col-span-2 md:row-span-2  md:mb-0 h-80"
+            className="p-2 mb-4 mt-4 text-black bg-white border-2 rounded-lg md:col-span-2 md:row-span-2  md:mb-0"
             style={{
-              backgroundImage: 'url("../images/bwMountains.png")',
+              backgroundImage: 'url("../images/groupShot01.png")',
               backgroundSize: "cover",
+              height: "34rem",
             }}
           ></div>
           <div>
             <h3>Where Do We Ride?</h3>
             <p>
-              All over the place! Sort of. Our "usual" ride spots are in
-              Pennsylvania locations such as anthracite outdoor adventure area.
-              But we've been known to stray.... We've ridden some of the best
-              trails in California, Pennsylvania, Georgia, New York, New Jersey,
-              and even Tennessee.
+              All over the place! Sort of. Our &quot;usual&quot; ride spots are
+              in Pennsylvania locations such as anthracite outdoor adventure
+              area. But we&apos;ve been known to stray.... We&apos;ve ridden
+              some of the best trails in California, Pennsylvania, Georgia, New
+              York, New Jersey, and even Tennessee.
             </p>
             <h3>When Do Ride?</h3>
             <p>
-              All the time! Kinda. We've ridden in all four seasons and have
-              braved all of the elements.{" "}
+              All the time! Kinda. We&apos;ve ridden in all four seasons and
+              have braved all of the elements.{" "}
             </p>
             <h3>Do We Want You to Join Us?</h3>
             <p>
-              Absolutely! We're actually social creatures and we hang out alot
-              on Facebook, Youtube, TikTok, Instagram (all of our links are in
-              the footer of this page).
+              Absolutely! We&apos;re actually social creatures and we hang out
+              alot on Facebook, Youtube, TikTok, Instagram (all of our links are
+              in the footer of this page).
             </p>
           </div>
         </div>
